294aeab87487e672a6186dcad57acb39b2588b96
Thorsten Schubert <tschubert@bafh.org>
2018-03-22 00:57:39 +0100

    introduced revision environment variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 set -e
 
+log()
+{
+    echo \[$(date +'%Y%m%d %H:%M:%S.%3N')\] \[${HOSTNAME}\] $*
+}
+
 echo ${TZ} > /etc/timezone
 dpkg-reconfigure -f noninteractive tzdata
-chown -vR ${UID}:${GID} /run/prosody /etc/prosody /var/lib/prosody /var/log/prosody /usr/lib/prosody /usr/src/prosody
+chown -R ${UID}:${GID} /run/prosody /etc/prosody /var/lib/prosody /var/log/prosody /usr/lib/prosody /usr/src/prosody
 
 if [[ "${LUAJIT,,}" = "true" ]]; then
+	log "installing luajit as default interpreter"
 	update-alternatives --install /usr/bin/lua lua-interpreter /usr/bin/luajit 150 \
 	&& update-alternatives --set lua-interpreter /usr/bin/luajit
 fi
@@ -13,13 +19,25 @@ fi
 if [[ ! -e /usr/src/prosody/.hg ]]; then
 	su-exec $UID:$GID hg clone https://hg.prosody.im/trunk /usr/src/prosody
 else
-	su-exec $UID:$GID hg pull -u -R /usr/src/prosody
+	su-exec $UID:$GID /bin/bash -c 'hg pull -u -R /usr/src/prosody \
+		&& hg update -C -R /usr/src/prosody'
+
+	if [[ -v PROSODY_REVISION ]]; then
+		log "checking out prosody revision: ${PROSODY_REVISION}"
+		su-exec $UID:$GID hg update -C -r ${PROSODY_REVISION} -R /usr/src/prosody
+	fi
 fi
 
 if [[ ! -e /usr/lib/prosody/modules-extra/.hg ]]; then
 	su-exec $UID:$GID hg clone https://hg.prosody.im/prosody-modules /usr/lib/prosody/modules-extra
 else
-	su-exec $UID:$GID hg pull -u -R /usr/lib/prosody/modules-extra
+	su-exec $UID:$GID /bin/bash -c 'hg pull -u -R /usr/lib/prosody/modules-extra \
+		&& hg update -C -R /usr/lib/prosody/modules-extra'
+
+	if [[ -v MODULES_REVISION ]]; then
+		log "checking out module revision: ${MODULES_REVISION}"
+		su-exec $UID:$GID hg update -C -r ${MODULES_REVISION} -R /usr/lib/prosody/modules-extra
+	fi
 fi
 
 # prosodyctl cert import complains if not owned by executing user (root)
